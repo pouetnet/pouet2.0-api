831dea5d3feb385c2894acab8ae633c800db7ba5
Gargaj <gargaj@conspiracy.hu>
2014-12-29 17:11:38 +0100

    add search api

--- _rewrite.php
+++ _rewrite.php
@@ -5,6 +5,7 @@ require(POUETAPI_POUET_ROOT_LOCAL . "/bootstrap.inc.php");
 $r = new Rewriter();
 $r->addRules(array(
   "^\/+v1\/prod\/?$" => "v1_prod.php",
+  "^\/+v1\/search\/prod\/?$" => "v1_search_prod.php",
 ));
 $r->addBootstrap("./functions.inc.php");
 $r->addEntryPoint("POUET_API");
---
+++ v1_search_prod.php
@@ -0,0 +1,40 @@
+<?
+if (!defined("POUET_API")) exit();
+
+//$prod = $_GET["id"] ? PouetProd::Spawn((int)$_GET["id"]) : null;
+$terms = split_search_terms( $_GET["q"] );
+if ($terms)
+{
+  $s = new BM_Query("prods");
+  $s->AddField("cmts.c as commentCount");
+  $s->AddJoin("left","(select which, count(*) as c from comments group by which) as cmts","cmts.which = prods.id");
+  $s->AddOrder("prods.name ASC");
+  $s->AddOrder("prods.id");
+  foreach($terms as $term)
+    $s->AddWhere(sprintf_esc("prods.name LIKE '%%%s%%'",_like($term)));
+  $prods = $s->perform();
+}
+if($prods)
+{
+  PouetCollectPlatforms( $prods );
+  PouetCollectAwards( $prods );
+  foreach($prods as &$prod)
+  {
+    unset($prod->views);
+    unset($prod->latestip);
+    unset($prod->addeduser->lastLogin);
+  }
+}
+
+$result = new stdClass();
+if ($prod)
+{
+  $result->success = true;
+  $result->results = $prods;
+}
+else
+{
+  $result->error = true;
+}
+output($result);
+?>
