d579786a23c9cac1ae6886aa4eb9fcc1f9f154ba
Gargaj <gargaj@conspiracy.hu>
2015-08-11 00:16:31 +0200

    add party API

---
+++ v1_party.php
@@ -0,0 +1,45 @@
+<?
+if (!defined("POUET_API")) exit();
+
+$party = PouetParty::spawn($_GET["id"]);
+$year = (int)$_GET["year"];
+
+$result = new stdClass();
+if ($party)
+{
+  $result->success = true;
+  $result->party = $party;
+
+  $prods = array();
+  $s = new BM_Query("prods");
+  $s->AddWhere( sprintf_esc("(prods.party = %d AND prods.party_year = %d) or (prodotherparty.party = %d AND prodotherparty.party_year = %d)",$party->id,$year,$party->id,$year) );
+  
+  // this is where it gets nasty; luckily we can fake it relatively elegantly: ORM won't notice if we override some of the field selections
+  $s->AddJoin("left","prodotherparty",sprintf_esc("prodotherparty.prod = prods.id and (prodotherparty.party = %d AND prodotherparty.party_year = %d)",$party->id,$year));
+  foreach($s->fields as &$v)
+  {
+    if ($v == "prods.party_compo as prods_party_compo")
+    {
+      $v = "COALESCE(prodotherparty.party_compo,prods.party_compo) as prods_party_compo";
+    }
+    if ($v == "prods.party_place as prods_party_place")
+    {
+      $v = "COALESCE(prodotherparty.party_place,prods.party_place) as prods_party_place";
+    }
+  }
+  
+  $result->prods = $s->perform();
+  
+
+  $inv = new BM_Query("prods");
+  $inv->AddWhere( sprintf_esc("(prods.invitation = %d AND prods.invitationyear = %d)",$this->party->id,$this->year,$this->party->id,$this->year) );
+  $inv->AddOrder( "prods.addedDate" );
+  $result->invitations = $inv->perform();
+
+}
+else
+{
+  $result->error = true;
+}
+output($result);
+?>
