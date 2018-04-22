984b0d2c57bb420c7d20660ffaed7b1824a3a089
Gargaj <gargaj@conspiracy.hu>
2018-04-22 15:17:32 +0200

    how did this get here huh

--- v1_stats.php
+++ v1_stats.php
@@ -1,16 +1,6 @@
 <?
 if (!defined("POUET_API")) exit();
 
-$group = $_GET["id"] ? PouetGroup::Spawn((int)$_GET["id"]) : null;
-if ($group)
-{
-  $s = new BM_Query("prods");
-  $s->AddWhere(sprintf_esc("(prods.group1 = %d) or (prods.group2 = %d) or (prods.group3 = %d)",$group->id,$group->id,$group->id));
-  $prods = $s->perform();
-  PouetCollectPlatforms($prods);
-  PouetCollectAwards($prods);
-}
-
 $result = new stdClass();
 
 $data = unserialize( file_get_contents( POUETAPI_POUET_ROOT_LOCAL . "/cache/pouetbox_stats.cache" ) );
