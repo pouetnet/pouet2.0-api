ee46c6e2628d457c0f5519410f4a4e2600326177
Gargaj <gargaj@conspiracy.hu>
2023-12-05 21:33:31 +0100

    add credits to api response

--- v1_user.php
+++ v1_user.php
@@ -8,6 +8,28 @@ if ($user)
 {
   $result->success = true;
   $result->user = $user->ToAPI();
+  
+  $s = new BM_Query();
+  $s->AddTable("credits");
+  $s->AddField("credits.role");
+  $s->Attach(array("credits"=>"prodID"), array("prods as prod"=>"id"));
+  $s->AddWhere(sprintf("credits.userID = %d",$user->id));
+  $s->AddOrder("credits_prod.releaseDate desc");
+  $data = $s->perform();
+
+  $prods = array();
+  foreach($data as $v) $prods[] = &$v->prod;
+  PouetCollectPlatforms($prods);
+  PouetCollectAwards($prods);
+
+  if ($prods)
+  {
+    $result->user["credits"] = array();
+    foreach($prods as $prod)
+    {
+      $result->user["credits"][] = $prod->ToAPI();
+    }
+  }
 }
 else
 {
